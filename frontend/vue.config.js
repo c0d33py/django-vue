bc5f9b77d1776afdf6e9abadb5ef84b19f65d93b
c0d33py <rootkill007@gmail.com>
2022-11-12 04:19:38 +0500

    first commit

---
+++ frontend/vue.config.js
@@ -0,0 +1,30 @@
+const BundleTracker = require("webpack-bundle-tracker");
+
+module.exports = {
+  publicPath: "http://0.0.0.0:8080",
+  outputDir: "./dist/",
+
+  chainWebpack: (config) => {
+    config.optimization.splitChunks(false);
+
+    config.plugin("BundleTracker").use(BundleTracker, [
+      {
+        filename: "./webpack-stats.json",
+      },
+    ]);
+
+    config.resolve.alias.set("__STATIC__", "static");
+
+    config.devServer
+      .host("0.0.0.0")
+      .port(8080)
+      .https(false)
+      .headers({ "Access-Control-Allow-Origin": ["*"] });
+  },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+};
